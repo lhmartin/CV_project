3acab2aa433831cc1d0e79d8954145fbda50b3eb
Luke Martin <lukechile@gmail.com>
2019-12-18 16:44:54 -0500

    updating scripts

--- sslime/sslime/data/ssl_transforms/__init__.py
+++ sslime/sslime/data/ssl_transforms/__init__.py
@@ -9,13 +9,16 @@
 import torchvision.transforms as transforms
 
 from sslime.data.ssl_transforms.ssl_rotate import SSL_IMG_ROTATE
-from sslime.data.ssl_transforms.jigsaw_trans import JIG_SAW_TRANS
+from sslime.data.ssl_transforms.jigsaw_trans_16 import JIG_SAW_TRANS_16
+from sslime.data.ssl_transforms.jigsaw_trans_40 import JIG_SAW_TRANS_40
+from sslime.data.ssl_transforms.jigsaw_trans_80 import JIG_SAW_TRANS_80
+from sslime.data.ssl_transforms.jigsaw_trans_120 import JIG_SAW_TRANS_120
 from sslime.data.ssl_transforms.basic_transforms_wrapper import (
     TORCHVISION_TRANSFORMS,
     TorchVisionTransformsWrapper,
 )
 
-TRANSFORMS = {"ssl_rotate": SSL_IMG_ROTATE, "JigSawTransform" : JIG_SAW_TRANS}
+TRANSFORMS = {"ssl_rotate": SSL_IMG_ROTATE, "JigSawTransform16" : JIG_SAW_TRANS_16, "JigSawTransform_40" : JIG_SAW_TRANS_40, "JigSawTransform_80" : JIG_SAW_TRANS_80, "JigSawTransform_120" : JIG_SAW_TRANS_120}
 
 
 def get_transform(transforms_list):
