008bcdf9550ccb799be580fa4632a904961f8476
Luke Martin <lukechile@gmail.com>
2019-12-19 18:39:52 -0500

    remove dead files

--- JigsawPuzzlePytorch-master/run_jigsaw_training.sh
+++
@@ -1,8 +0,0 @@
-IMAGENET_FOLD=../data
-
-GPU=${1} # gpu used
-CHECKPOINTS_FOLD=${2} #path_to_output_folder
-
-#python JigsawTrain.py ${IMAGENET_FOLD} --checkpoint=${CHECKPOINTS_FOLD} \
-#                      --classes=1000 --batch 128 --lr=0.001 --gpu=${GPU} --cores=10
-python3 JigsawTrain.py ${IMAGENET_FOLD} --classes=40 --batch 128 --lr=0.01 --cores=10 --epochs 100 --rot 1
